56a6ef030f6fb8d5c07c33c0bb30ddbc4671abf9
Thomas Mutter <thomas.mutter@nedap.com>
2020-01-26 09:18:15 +0100

    Server and Client protocol interfaces written

--- src/goProtocol/GoClientProtocol.java
+++ src/goProtocol/GoClientProtocol.java
@@ -2,6 +2,22 @@ package goProtocol;
 
 public interface GoClientProtocol {
 
-	public String handshake();
+	/**
+	 * Send single line in the form of H;version;name;color
+	 */
+	public void doHandshake();
 
+	/**
+	 * Client sends suggested move as an integer to server in the form M;move upon
+	 * request of the server.
+	 * 
+	 * Upon receiving a move request the server responds with validity of move
+	 */
+	public String doMove(int move);
+
+	/**
+	 * Client quits the game and terminates connection by sending Q to the server
+	 * 
+	 */
+	public void quitGame();
 }
--- src/goProtocol/GoServerProtocol.java
+++ src/goProtocol/GoServerProtocol.java
@@ -2,6 +2,47 @@ package goProtocol;
 
 public interface GoServerProtocol {
 
-	public String handshake();
+	/**
+	 * After receiving a request to play from a client respond with
+	 * 
+	 * @return H;version;(optional free message)
+	 */
+	public String respondToHandshake();
+
+	/**
+	 * When two clients have connected the server starts the game. Both clients
+	 * receive a notification: G;board;color
+	 */
+	public void startGame();
+
+	/**
+	 * Give turn to client in the form: T;board;opponentsLastMove, opponentsLastMove
+	 * is either pass or null at start of game
+	 */
+	public void giveTurnToMove(String board);
+
+	/**
+	 * After receiving move from client this response is sent if the move was valid
+	 * 
+	 * @return R;V;board
+	 */
+	public String validMove();
+
+	/**
+	 * After receiving move from client this response is sent if the move was valid
+	 * 
+	 * @return R;I;(optional free message)
+	 */
+	public String invalidMove();
+
+	/**
+	 * The game can end for several reasons, when it does so. The server sends to
+	 * clients E;reason;winner;scoreBlack;scoreWhite
+	 * 
+	 * Reasons can be: [F]inished, [C]heated, [D]isconnect from other client, E[X]it
+	 * from other client.
+	 * 
+	 */
+	public void endGame();
 
 }
